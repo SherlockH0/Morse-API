b6597785f62b5a6fb1a6f42fedee3ac904073cdf
SherlockH0 <ddmlf7@gmail.com>
2024-10-11 18:17:37 +0300

    Add user profile creation

--- morse_api/api/apps.py
+++ morse_api/api/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "morse_api.api"
+
+    def ready(self):
+        import morse_api.api.signals
